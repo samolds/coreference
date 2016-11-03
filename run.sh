570e9577a336b0e7586c41a8a675557ce469fa13
Sam Olds <samolds@gmail.com>
2016-11-03 00:24:23 -0600

    very small example of tokenizing a data text file using nltk

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python src/main.py data/dev/b1.crf data/dev/b1.txt
+python src/main.py data/dev/b1.txt
--- src/main.py
+++ src/main.py
@@ -4,8 +4,31 @@ from nltk.tokenize import TreebankWordTokenizer
 import sys
 
 
+# builds a list of tokens by each line
+# param:  data_file: file handler
+# return: list of list of tokens
+def build(data_file):
+  tokens_by_line = []
+  for line in data_file:
+    tokens = TreebankWordTokenizer().tokenize(line)
+    if len(tokens) > 0:
+      tokens_by_line.append(tokens)
+  return tokens_by_line
+
+
+# main "method" that kicks off various routines
 if __name__ == "__main__":
-  s = '''Good muffins cost $3.88\nin New York.  
-  Please buy me\ntwo of them.\nThanks.'''
-  x = TreebankWordTokenizer().tokenize(s)
-  print "\theyo %s\n\t%s" % (" ".join(sys.argv[1:]), x)
+  if not len(sys.argv) == 2:
+    print "expected usage:\n\tpython main.py <input file>"
+    exit(1)
+
+  # open files
+  data_file = open(sys.argv[1], 'r')
+
+  # build structures from corresponding input files
+  tokens_by_line = build(data_file)
+  for tokens in tokens_by_line:
+    print ":".join(tokens)
+
+  # close files
+  data_file.close()
